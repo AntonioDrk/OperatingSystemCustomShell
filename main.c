fda74790f01d50c7cc2073dbddfc7f8376944fc6
Remus-Antonio <ekro98@gmail.com>
2019-01-12 18:42:18 +0200

    Added function to split arguments

--- main.c
+++ main.c
@@ -3,6 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
 #define SUCCES_FLAG 0
+#define EXIT_ERROR -1
+#define MAXLENGTH_TOK_ARRAY 32
+#define TOKEN_DELIMITATORS " \t\a\n\r"
+
+// Splits the arguments into a vector and returns it
+char ** splitShellLine(char* line){
+  // The length of the token array, this will be incremented
+  // dynamically, if it needs to
+  int maxLength = MAXLENGTH_TOK_ARRAY;
+  int index = 0;
+
+  char ** tokenArray = malloc(maxLength * sizeof(char*));
+  char *token;
+
+  if(tokenArray == NULL){
+    fprintf(stderr, "BabyShell: Error while trying to allocate tokenArray \n");
+    exit(EXIT_ERROR);
+  }
+
+  token = strtok(line,TOKEN_DELIMITATORS);
+  while(token != NULL){
+    tokenArray[index] = token;
+    index++;
+
+    if(index >= maxLength){
+      maxLength += MAXLENGTH_TOK_ARRAY;
+      tokenArray = realloc(tokenArray, maxLength * sizeof(char*));
+
+      if(tokenArray == NULL){
+        fprintf(stderr, "BabyShell: Error while trying to allocate tokenArray \n");
+        exit(EXIT_ERROR);
+      }
+    }
+
+    token = strtok(NULL,TOKEN_DELIMITATORS);
+  }
+  tokenArray[position] = NULL;
+  return tokenArray;
+}
 
 char* readShellLine(){
   char *line = NULL;
@@ -19,7 +58,7 @@ void shellLoop(){
 
   do {
     readLine = readShellLine();
-    // separate command and args
+    args = splitShellLine(readLine);
     // execute command with args
 
     free(readLine);
