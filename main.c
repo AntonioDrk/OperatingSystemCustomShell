f1663fc42f50df090315d4184af46ced0fbbf32f
Remus-Antonio <ekro98@gmail.com>
2019-01-13 23:45:13 +0200

    Update main.c (#4)

--- main.c
+++ main.c
@@ -8,18 +8,24 @@
 #define EXIT_ERROR -1337
 #define MAXLENGTH_TOK_ARRAY 32
 #define TOKEN_DELIMITATORS " \t\a\n\r"
+#define MAX_RECINPUT 100
 
 // Function Declarations for the builtin shell commands
 int shellHelp(char **args);
 int shellExit(char **args);
 int shellCd(char **args);
+int shellHistory(char **args);
 
 // List of builtin commands
 char *builtinStr[] = {
   "help",
   "exit",
-  "cd"
+  "cd",
+  "history"
 };
+
+int commandsCalled = 0;
+char *history[MAX_RECINPUT];
 
 // IMPORTANT: MAKE SURE THAT THEY ARE IN THE SAME POSITION IN BOTH ARRAYS!!!
 
@@ -27,7 +33,8 @@ char *builtinStr[] = {
 int (*builtinFunc[]) (char **) = {
   &shellHelp,
   &shellExit,
-  &shellCd
+  &shellCd,
+  &shellHistory
 };
 
 // Get the length of the commands array ()
@@ -40,7 +47,6 @@ int shellHelp(char **args){
   printf("BabyShell 0.1 \n");
   printf("This shell was made by: George, Ema, Antonio as a project for Operating Systems class.\n");
   printf("Type a programs name and it's arguments into the terminal and hit enter\n");
-  printf("You can use \";\" to execute multiple commands in order and also we tried to implement piping with \"|\" but it doesn't work so don't try it!\n");
   printf("The following commands are built in:\n");
   for(int i = 0; i < shellNumBuiltin(); i++){
     printf("%s\t", builtinStr[i]);
@@ -113,6 +119,97 @@ int shellExecute(char **args){
   return shellLaunch(args);
 }
 
+int shellRecordInput(char *lineRead){ 
+    if(commandsCalled>=MAX_RECINPUT) 
+    {
+      commandsCalled = 0;
+      *history = malloc(MAX_RECINPUT * sizeof(char*));
+    }
+    history[commandsCalled] = malloc(strlen(lineRead));
+    strcpy(history[commandsCalled],lineRead);    
+    commandsCalled++;
+    return SUCCES_FLAG;
+}
+
+int shellHistory(char **args) {
+  for(int i=0;i<commandsCalled;i++)
+    {
+      printf("%s", history[i]);  
+    }
+	return SUCCES_FLAG;
+}
+
+int spawn_proc (int in, int out, char **args)
+{
+  pid_t pid;
+
+  if ((pid = fork ()) == 0)
+    {
+      if (in != 0)
+        {
+          dup2 (in, 0);
+          close (in);
+        }
+
+      if (out != 1)
+        {
+          dup2 (out, 1);
+          close (out);
+        }
+
+      return execvp (args[0], args);
+    }
+
+  return pid;
+}
+
+int fork_pipes (int n,char ***commands)
+{
+  int i;
+  pid_t pid;
+  int in, fd [2];
+
+  /* The first process should get its input from the original file descriptor 0.  */
+  in = 0;
+
+  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
+  for (i = 0; i < n - 1; ++i)
+    {
+      pipe (fd);
+
+      /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
+      spawn_proc (in, fd [1], commands[i]);
+
+      /* No need for the write end of the pipe, the child will write here.  */
+      close (fd [1]);
+
+      /* Keep the read end of the pipe, the next child will read from there.  */
+      in = fd [0];
+    }
+
+  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
+     and output to the original file descriptor 1. */  
+  if (in != 0)
+    dup2 (in, 0);
+
+  /* Execute the last stage with the current process. */
+  return execvp (commands[n-1][0], commands[n-1]);
+}
+
+int shellPipe(char **args) {
+	char ***commands;
+	int n = 0;
+	char **token = strtok(args,"|");
+	while(token!=NULL) {
+		commands = realloc(commands, (n+1)*sizeof(char**));
+		strcat(commands[n], token);
+		n++;
+		
+		token=strtok(args,"|");
+	}
+	fork_pipes(n, commands);
+}
+
 // Splits the arguments into a vector and returns it
 char ** splitShellLine(char* line){
   // The length of the token array, this will be incremented
@@ -157,98 +254,7 @@ char* readShellLine(){
   return line;
 }
 
-int spawn_proc (int in, int out, char **args)
-{
-  if (in != 0)
-  {
-    dup2 (in, 0);
-    close (in);
-  }
-
-  if (out != 1)
-  {
-    dup2 (out, 1);
-    close (out);
-  }
-
-  return shellExecute(args);
-}
-
-int fork_pipes (int n, char ***commands)
-{
-  int i;
-  pid_t pid;
-  int in, fd [2];
-
-  /* The first process should get its input from the original file descriptor 0.  */
-  in = 0;
-
-  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
-  for (i = 0; i < n - 1; ++i)
-  {
-    pipe(fd);
-
-    /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
-    spawn_proc (in, fd [1], commands[i]);
-
-    /* No need for the write end of the pipe, the child will write here.  */
-    close (fd [1]);
-
-    /* Keep the read end of the pipe, the next child will read from there.  */
-    in = fd [0];
-  }
-
-  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
-     and output to the original file descriptor 1. */
-  if (in != 0)
-    dup2 (in, 0);
-
-  /* Execute the last stage with the current process. */
-  return shellExecute(commands[n-1]);
-}
-
-int shellPipe(char *readLine) {
-  int length = strlen(readLine);
-  printf("DEBUG: %s\n",readLine);
-	char ***commands = NULL;
-	int commandsIndex = 0;
-  char *commandBlock = NULL;
-	//char *token = strtok(tmpLine,"|");
-
-
-
-	for(int i = 0, index = 0; i < length; index++ ,i++){
-    if(readLine[i] != '|' && i != length - 1){
-      commandBlock = realloc(commandBlock,index + 1);
-      commandBlock[index] = readLine[i];
-    }else{
-      commands = realloc(commands, sizeof(char**) * (commandsIndex + 1));
-      char *savedCommand = malloc(strlen(commandBlock));
-      strcpy(savedCommand,commandBlock);
-      commands[commandsIndex] = splitShellLine(savedCommand);
-      commandsIndex++;
-      free(commandBlock);
-      commandBlock = NULL;
-      index = 0;
-    }
-	}
-
-
-//	while(token!=NULL) {
-//    fprintf(stdout,"DEBUG: %d\n", index);
-//		commands = realloc(commands, (index+1)*sizeof(char**));
-//
-//		char* tempLine = malloc(strlen(token));
-//		strcpy(tempLine,token);
-//    commands[index] = splitShellLine(tempLine);
-//
-//    index++;
-//		token=strtok(NULL,"|");
-//	}
-	fork_pipes(commandsIndex, commands);
-}
-
-
+// TODO: TEST ME WHEN POSSIBLE
 int executeMultipleCommands(char* args){
 
   int length = strlen(args);
@@ -259,15 +265,20 @@ int executeMultipleCommands(char* args){
     exit(EXIT_ERROR);
   }
 
-
   int index = 0;
+  int status = 1;
+
   for(int i = 0; i < length; i++ , index++){
     if(args[i] == ';' || i == length - 1){
 
       command[index] = '\0';
-      shellExecute(splitShellLine(command));
+      status = shellExecute(splitShellLine(command));
 
       free(command);
+
+      if(status != SUCCES_FLAG){
+        return status;
+      }
 
       index = -1;
       if(i != length - 1){
@@ -287,33 +298,30 @@ int executeMultipleCommands(char* args){
 
 
 void shellLoop(){
-  char *readLine, *processedLine;
+  char *readLine;
   char **args;
   int status = 1;
 
   do {
     printf("> ");
     readLine = readShellLine();
-    processedLine = malloc(strlen(readLine));
-    strcpy(processedLine, readLine);
-
-    args = splitShellLine(processedLine);
+    shellRecordInput(readLine);
 
     // If in the read line there is a ; we parse it as multiple commands
     if(strchr(readLine,';') != NULL){
       status = executeMultipleCommands(readLine);
-    }else if(strchr(readLine,'|') != NULL){
-      status = shellPipe(readLine);
     }else{
+      args = splitShellLine(readLine);
       status = shellExecute(args);
+      free(args);
     }
-    free(args);
+
     free(readLine);
   } while(status);
 }
 
 int main(int argc, char const *argv[]) {
-
+ 
   // Loop used to always await input from user and execute multiple commands
   shellLoop();
 
