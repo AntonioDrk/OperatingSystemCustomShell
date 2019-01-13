09ff5d847e76f40525204d6f21f7ca6ef04e31da
Remus-Antonio <ekro98@gmail.com>
2019-01-13 19:42:46 +0200

    Added pipeline a WIP pipeline

--- main.c
+++ main.c
@@ -112,6 +112,77 @@ int shellExecute(char **args){
   return shellLaunch(args);
 }
 
+int spawn_proc (int in, int out, char **args)
+{
+  pid_t pid;
+
+  if ((pid = fork ()) == 0)
+    {
+      if (in != 0)
+        {
+          dup2 (in, 0);
+          close (in);
+        }
+
+      if (out != 1)
+        {
+          dup2 (out, 1);
+          close (out);
+        }
+
+      return execvp (args[0], args);
+    }
+
+  return pid;
+}
+
+int fork_pipes (int n, ***commands)
+{
+  int i;
+  pid_t pid;
+  int in, fd [2];
+
+  /* The first process should get its input from the original file descriptor 0.  */
+  in = 0;
+
+  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
+  for (i = 0; i < n - 1; ++i)
+    {
+      pipe (fd);
+
+      /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
+      spawn_proc (in, fd [1], commands[i]);
+
+      /* No need for the write end of the pipe, the child will write here.  */
+      close (fd [1]);
+
+      /* Keep the read end of the pipe, the next child will read from there.  */
+      in = fd [0];
+    }
+
+  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
+     and output to the original file descriptor 1. */  
+  if (in != 0)
+    dup2 (in, 0);
+
+  /* Execute the last stage with the current process. */
+  return execvp (commands[n-1][0], commands[n-1]);
+}
+
+int shellPipe(char **args) {
+	char ***commands;
+	int n = 0;
+	char **token = strtok(args,"|");
+	while(token!=NULL) {
+		commands = realloc(commands, (n+1)*sizeof(char**));
+		strcat(commands[n], token);
+		n++;
+		
+		token=strtok(args,"|");
+	}
+	fork_pipes(n, commands);
+}
+
 // Splits the arguments into a vector and returns it
 char ** splitShellLine(char* line){
   // The length of the token array, this will be incremented
